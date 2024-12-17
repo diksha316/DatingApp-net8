f371848ded7e0995949966336fe98e3f7cdcd648
diksha316 <diksjais414@gmail.com>
2024-12-17 12:50:26 +0530

    "End of Section3" End of Section3

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -11,10 +11,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
 }
 );
 
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+//Adding CORS as a middleware
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200","https://localhost:4200"));
+
 app.MapControllers();
+
 app.Run();
 
 
